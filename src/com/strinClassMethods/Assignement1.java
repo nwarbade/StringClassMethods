25a64286419e77a7b90d51273d74cc916014bac3
nwarbade <nwarbade@gmail.com>
2023-05-19 15:47:39 +0530

    Initial commit

---
+++ src/com/strinClassMethods/Assignement1.java
@@ -0,0 +1,20 @@
+package com.strinClassMethods;
+
+public class Assignement1 {
+	
+//	Assignment: WAP to replace all the space value from the String- This is String
+	
+	public static void main(String[] args) {
+		
+		String s1 = "my name is nana";
+		
+		//String s2 = s1.replace("my name is nana", "mynameisnana");
+		
+		String s3 = s1.replace(" ", "");
+		
+		System.out.println(s3);
+		
+		
+	}
+
+}
---
+++ src/com/strinClassMethods/Assignement2.java
@@ -0,0 +1,14 @@
+package com.strinClassMethods;
+
+public class Assignement2 {
+	
+	//Assignment: WAP to count number of upper case characters present inside the String
+	
+	public static void main(String[] args) {
+		
+		String s1 = "My Name Is Nana";
+		
+		
+	}
+
+}
