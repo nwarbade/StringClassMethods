a03c285e5ee5540b1e4b6be96ed436a45a7ef73f
nwarbade <nwarbade@gmail.com>
2023-05-26 15:03:18 +0530

    New Methods Added
    remaining methods added

--- src/com/strinClassMethods/StringMethods.java
+++ src/com/strinClassMethods/StringMethods.java
@@ -115,12 +115,96 @@ public class StringMethods {
 	boolean endswith = s19.endsWith("ers");
 
 	System.out.println(endswith);//false
+	
+//	split: 
+	
+	String s20 = "Today is saturday";
+	String[] s21 = s20.split(" ");
+	
+	
+	
+	for(String s22:s21)
+	{
+		System.out.println(s22);
+	}
+	
+	
+	
+//	WAP to remove the un - ncessarily added space from the Sting- "   this     is String"
+	
+	
+	String s23 = "kjashdjsSDAnjnASDSA";
+	
+	
+	String s24 = s23.replaceAll("[A-Z]", "");
+	
+	System.out.println(s24);//kjashdjsnjn
+	
+	String s25 = s23.replaceAll("[a-z]", "");
+	System.out.println(s25);//SDAASDSA
+	
+	
+	String s26 = s23.replaceAll("[A-C]", "");
+	
+	System.out.println(s26);//kjashdjsSDnjnSDS
+	
+	String s27 = "AccEn%t7ure";
+	
+	String s28 = s27.replaceAll("[^A-Za-z]", "");
+	
+	System.out.println(s28);//AccEnture
+	
+	
+
+//	WAP to remove the digits from the String
+
+	String s29 = s27.replaceAll("[0-9]", "");
+
+	System.out.println(s29);
 
 
+	String s30 = s27.replaceAll("[^A-Za-z0-9]", "");
+
+	System.out.println(s30);//AccEnt7ure
 
 
+	char [] chararray = s30.toCharArray();
 
+	for(char ch :chararray)
+	{
+		System.out.println(ch);
+	}
+
+//	Non primitive to primitive
+
+	String s31 = "10";
+
+	int s32 = Integer.parseInt(s31);//10
+
+	System.out.println(s32);
+
+	String s33 = "58.56";
+
+	double s34 = Double.parseDouble(s33);
+
+	System.out.println(s34);//58.56
+
+
+//	Primitive to non primitive:
+
+	int s35 = 40;
+
+	String s36 = String.valueOf(s35);
+
+	System.out.println(s36+5);//405
+
+	boolean s37 = true;
+
+	String s38 = String.valueOf(s37);
+
+	System.out.println(s38);//true
+	
 	}
 
 
-}
+	}
